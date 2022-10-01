0f867489b79b9e04e2055e16755345c2af2e439e
Rahmat Ullah <rahmatbd1533@gmail.com>
2022-10-01 12:32:26 +0600

    share one function between two input field created

--- js/deposit.js
+++ js/deposit.js
@@ -1,8 +1,17 @@
 function getInputValueById(inputId){
     const inputField = document.getElementById(inputId);
-    return inputField;
+    const inputFieldValueString = inputField.value;
+    const inputFieldValue = parseFloat(inputFieldValueString);
+    inputField.value = ('');
+    return inputFieldValue;
+}
+
+function getElementValueById(elementsId){
+    const element = document.getElementById(elementsId);
+    const elementValueString = element.innerText;
+    const value = parseFloat(elementValueString);
 }
 
 document.getElementById('btn-deposit').addEventListener('click', function(){
-
+    const newDepositAmount = getInputValueById('deposit-field');
 })
